6a4e203f98358c60b2c86deee06769cb9f6396da
SMRist <s.m.r.00800ist@gmail.com>
2024-06-19 21:25:16 +0300

    Initial commit

---
+++ 2/List.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <iostream>
+struct ListNode
+{
+public:
+    ListNode(int value, ListNode* prev = nullptr, ListNode* next = nullptr)
+        : value(value), prev(prev), next(next)
+    {
+        if (prev != nullptr) prev->next = this;
+        if (next != nullptr) next->prev = this;
+    }
+
+public:
+    int value;
+    ListNode* prev;
+    ListNode* next;
+};
+
+
+class List
+{
+public:
+    List()
+        : m_head(new ListNode(static_cast<int>(0))), m_size(0),
+        m_tail(new ListNode(0, m_head))
+    {
+    }
+
+    virtual ~List();
+
+    bool Empty();
+
+    unsigned long Size();
+
+    void PushFront(int value);
+
+    void PushBack(int value);
+
+    int PopFront();
+
+    int PopBack();
+
+    void Clear();
+
+private:
+    ListNode* extractPrev(ListNode* node)
+    {
+        auto target = node->prev;
+        target->prev->next = target->next;
+        target->next->prev = target->prev;
+        --m_size;
+        return target;
+    }
+
+private:
+    ListNode* m_head;
+    ListNode* m_tail;
+    unsigned long m_size;
+};
--- 2/main.cpp
+++ 2/main.cpp
@@ -1,131 +1,19 @@
+#include"List.h"
 #include <catch2/catch_test_macros.hpp>
-
-#include <iostream>
-
-struct ListNode
-{
-public:
-    ListNode(int value, ListNode* prev = nullptr, ListNode* next = nullptr)
-        : value(value), prev(prev), next(next)
-    {
-        if (prev != nullptr) prev->next = this;
-        if (next != nullptr) next->prev = this;
+TEST_CASE("ESDTest", "[List]") {
+    List List;
+    SECTION("Empty") {
+        INFO("Тест для Empty");
+        REQUIRE(List.Empty() == 1);
     }
-
-public:
-    int value;
-    ListNode* prev;
-    ListNode* next;
-};
-
-
-class List
-{
-public:
-    List()
-        : m_head(new ListNode(static_cast<int>(0))), m_size(0),
-        m_tail(new ListNode(0, m_head))
-    {
+    SECTION("Size") {
+        INFO("Тест для Size");
+        List.PushFront(5);
+        REQUIRE(List.Size() == 1);
     }
-
-    virtual ~List()
-    {
-        Clear();
-        delete m_head;
-        delete m_tail;
+    SECTION("Clear") {
+        INFO("Тест для Clear");
+        List.Clear();
+        REQUIRE(List.Size() == 0);
     }
-
-    bool Empty() { return m_size == 0; }
-
-    unsigned long Size() { return m_size; }
-
-    void PushFront(int value)
-    {
-        new ListNode(value, m_head, m_head->next);
-        ++m_size;
-    }
-
-    void PushBack(int value)
-    {
-        new ListNode(value, m_tail->prev, m_tail);
-        ++m_size;
-    }
-
-    int PopFront()
-    {
-        if (Empty()) throw std::runtime_error("list is empty");
-        auto node = extractPrev(m_head->next->next);
-        int ret = node->value;
-        delete node;
-        return ret;
-    }
-
-    int PopBack()
-    {
-        if (Empty()) throw std::runtime_error("list is empty");
-        auto node = extractPrev(m_tail);
-        int ret = node->value;
-        delete node;
-        return ret;
-    }
-
-    void Clear()
-    {
-        auto current = m_head->next;
-        while (current != m_tail)
-        {
-            current = current->next;
-            delete extractPrev(current);
-        }
-    }
-
-private:
-    ListNode* extractPrev(ListNode* node)
-    {
-        auto target = node->prev;
-        target->prev->next = target->next;
-        target->next->prev = target->prev;
-        --m_size;
-        return target;
-    }
-
-private:
-    ListNode* m_head;
-    ListNode* m_tail;
-    unsigned long m_size;
-};
-
-
-TEST_CASE("Функции PopBack, popFront, PushBack, PushFront", "[list]") {
-    List list;
-
-    REQUIRE_THROWS_AS(list.PopBack(), std::runtime_error);
-    REQUIRE_THROWS_AS(list.PopFront(), std::runtime_error);
-
-    list.PushBack(1);
-    list.PushBack(2);
-    list.PushFront(0);
-
-    REQUIRE(list.PopBack() == 2);
-    REQUIRE(list.Size() == 2);
-
-    REQUIRE(list.PopFront() == 0);
-    REQUIRE(list.Size() == 1);
-}
-
-TEST_CASE("Сложный сценарий", "[list]") {
-    List list;
-
-    for (int i = 0; i < 4; i++) {
-        list.PushBack(i);
-    }
-    REQUIRE(list.Size() == 4);
-
-    for (int i = 0; i < 2; i++) {
-        REQUIRE(list.PopFront() == i);
-        REQUIRE(list.PopBack() == 3 - i);
-    }
-
-    REQUIRE(list.Size() == 0);
-    REQUIRE(list.Empty() == 1);
 }
